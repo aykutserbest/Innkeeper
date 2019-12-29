b657f1c3e81e6df55c8dabd3884e60cb284191dc
aykutserbest <42496816+aykutserbest@users.noreply.github.com>
2019-12-30 01:21:43 +0300

    SulamaBox Ekleme

--- Innkeeper/Assets/Scripts/IrrigationBoxController.cs
+++ Innkeeper/Assets/Scripts/IrrigationBoxController.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class IrrigationBoxController : MonoBehaviour
 {
-    public GameSourceController
+    public GameSourceController source;
+    public GameObject plantAreaObject;
+    public FarmPlantArea plantedArea;
+    int level;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = GameObject.FindGameObjectWithTag("Scripts").GetComponent<GameSourceController>();
+        plantedArea = plantAreaObject.GetComponent<FarmPlantArea>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            level = plantedArea.plantLevel;
+            Debug.Log(level);
+
+
+
+        }
     }
 }
